d7b4bb66dc1e2c13db3ffb13f2aa9649aa30aa85
Mamatha kempeli 97 <157340718+Mamathakempeli@users.noreply.github.com>
2024-02-02 09:46:30 +0530

    Add files via upload

---
+++ iterate_loop_json_task.js
@@ -0,0 +1,40 @@
+
+var json = [{
+    "id" : "mamatha1", 
+    "msg"   : "For the given JSON iterate over all for loops (for, for in, for of, forEach)",
+    "task" : "day5_task",
+    "mail": "mamathakempeli97@gmail.com"
+},
+{
+    "id" : "Bindu", 
+    "msg"   : "For the given JSON iterate over all for loops (for, for in, for of, forEach)",
+    "task" : "Day5_task",
+    "mail": "mamathakempeli97@gmail.com"
+}];
+//for loop
+for(var i = 0; i < json.length; i++) {
+    var obj = json[i];
+
+    console.log(obj.id);
+    console.log(obj.msg);
+    console.log(obj.task);
+    console.log(obj.mail);
+
+}
+//for Each
+json.forEach(function(obj) { console.log(obj.msg); });
+
+//for In
+for (var key in json) {
+if (json.hasOwnProperty(key)) {
+  console.log(json[key].id);
+ 
+}
+}
+//for Of
+let text = "";
+for (let x of json[key].id) {
+ text += x; 
+}
+ console.log(text);
+
---
+++ my_resume_json_task.js
@@ -0,0 +1,64 @@
+let myResume={
+    "basics": {
+      "name": "Mamatha K",
+      "email": "mamathakempeli97@gamil.com",
+      "phone": 7093417661,
+      "degree": "Btech",
+      "location": {
+        "address": "Pungnur,chittor,AP",
+        "postalCode": 517247,
+        "city": "chittor",
+        "state": "AndhraPradesh",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/mamatha-k-5a3464265",
+          "github":"https://github.com/Mamathakempeli"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "The Photophilehub PVT",
+        "position": "Business Executive",
+        "startDate": "2020-11-04",
+        "endDate": "2021-08-15",
+        "summary": "i am the business executive handling various task and gaining lots of experience",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Mother Institute Of Engineering and technology",
+        "department": "Electronics",
+        "studyType": "fulltime",
+        "batch start year": 2014,
+        "batch end year": 2018,
+        "gpa": 7.2,
+      }
+    ],
+    "skills": [
+      {
+        "name": "c,c++,datastructures,javascript",
+        "level": "beginer",
+        "project": [
+          "Command based voice controlled robot"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Telugu,kannada,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "Playing with kids,watching TV, gardening",
+      }
+    ]
+  }
+  console.log(myResume);
+
+  
+  
+  
